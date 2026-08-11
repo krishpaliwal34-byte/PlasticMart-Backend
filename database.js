3f9d1ef4ab2cd0f592fa5244c1f0243748d9bbac
Krish Paliwal <krishpaliwal34@gmail.com>
2026-08-11 17:06:07 +0530

    Database Update

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async() => {
     try{
-        await mongoose.connect("mongodb://localhost:27017/PlasticMart")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Database is connect")
     }
     catch(err){
